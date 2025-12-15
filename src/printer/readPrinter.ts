c8b9855db761f88ee5bde184f3c61723bbbe4085
Carlos <carljavy@gmail.com>
2025-12-15 16:02:05 -0600

    function createPrinter

---
+++ src/printer/readPrinter.ts
@@ -0,0 +1,12 @@
+import { ThermalPrinter, PrinterTypes, CharacterSet, BreakLine } from "node-thermal-printer";
+
+export function createPrinter(){
+    return new ThermalPrinter({
+        type: PrinterTypes.EPSON,
+        interface: 'tcp://192.168.100.52',
+        characterSet: CharacterSet.PC852_LATIN2,
+        removeSpecialCharacters: false,
+        lineCharacter: "=",
+        breakLine: BreakLine.WORD
+    })
+}
